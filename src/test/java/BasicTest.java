9d40148b51f0a9f49a345b9932c961d1455b605a
Sean Pike <pike.sean@gmail.com>
2020-03-12 10:32:22 +0000

    Added selenium dependencies with webdriver test

--- src/test/java/BasicTest.java
+++ src/test/java/BasicTest.java
@@ -1,9 +1,16 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BasicTest {
     @Test
     public void simpleTest() {
-        Assertions.assertEquals(2, 1 + 1, "Simple test");
+        WebDriverManager.chromedriver().setup(); //gets matching version of the chrome driver for the test system
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+        Assertions.assertEquals("Google", driver.getTitle(), "Simple test");
+        driver.quit();
     }
 }
